8f954631ddae1a8c70935a90556eee60091ca386
alhatmii <alhatmii.150@gmail.com>
2023-01-23 09:36:21 +0400

    Added Invocie and Item Classes

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,9 +1,18 @@
 
 public class Invoice {
-	
+
 	private String firstName;
 	private String middleName;
 	private String lastName;
+	private String phoneNumber;
+	private String invoiceDate;
+	private int noOfItems;
+	private int totalAmount;
+	private double paidAmount; 
+	private double balance;
 
-	
+	void fullName() {
+		String fullName = null;
+		System.out.println(fullName);
+	}
 }
---
+++ src/Item.java
@@ -0,0 +1,8 @@
+
+public class Item {
+	 private String itemId;
+	 private String itemName;
+	 private String unitPrice;
+	 private int quantity;
+	 private double qtyPrice;
+}
