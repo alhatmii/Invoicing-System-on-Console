bd11379096137b19615e284acedbad9448459891
alhatmii <alhatmii.150@gmail.com>
2023-01-23 15:49:16 +0400

    Completion of Class : (Invoice & Item)

--- src/Invoice.java
+++ src/Invoice.java
@@ -100,62 +100,114 @@ public class Invoice {
 		String[] FirstName = new String[10];
 		FirstName[0] = "Mohammed";
 		FirstName[1] = "Ali";
-		FirstName[2] = "Cart";
-		FirstName[3] = "Dart";
-		FirstName[4] = "Electronics";
-		FirstName[5] = "Food";
-		FirstName[6] = "Golf";
-		FirstName[7] = "Horse";
-		FirstName[8] = "Dates";
-		FirstName[9] = "Rocks";
+		FirstName[2] = "Shaima";
+		FirstName[3] = "Fatima";
+		FirstName[4] = "Abdulhameed";
+		FirstName[5] = "Adnan";
+		FirstName[6] = "Malak";
+		FirstName[7] = "Aisha";
+		FirstName[8] = "Said";
+		FirstName[9] = "Sami";
 
-//		Array of Items ID:
-		String[] ItemId = new String[10];
-		ItemId[0] = "A0235";
-		ItemId[1] = "B9575";
-		ItemId[2] = "C9523";
-		ItemId[3] = "D4482";
-		ItemId[4] = "E3256";
-		ItemId[5] = "F0125";
-		ItemId[6] = "G9856";
-		ItemId[7] = "H9342";
-		ItemId[8] = "D0321";
-		ItemId[9] = "R3597";
+//		Array of customer second Names:
+		String[] MiddleName = new String[10];
+		MiddleName[0] = "Hisham";
+		MiddleName[1] = "Nasser";
+		MiddleName[2] = "Ali";
+		MiddleName[3] = "Suleiman";
+		MiddleName[4] = "Mansoor";
+		MiddleName[5] = "Ismail";
+		MiddleName[6] = "Ahmed";
+		MiddleName[7] = "Mohammed";
+		MiddleName[8] = "Abdulaziz";
+		MiddleName[9] = "Majid";
 
-//		Array of Items ID:
-		Double[] unitPrice = new Double[10];
-		unitPrice[0] = 3.500;
-		unitPrice[1] = 6.500;
-		unitPrice[2] = 9.400;
-		unitPrice[3] = 200.100;
-		unitPrice[4] = 65.300;
-		unitPrice[5] = 6.500;
-		unitPrice[6] = 69.300;
-		unitPrice[7] = 3000.200;
-		unitPrice[8] = 362.200;
-		unitPrice[9] = 21.200;
+//		Array of customer last Names:
+		String[] LastName = new String[10];
+		LastName[0] = "Khamis";
+		LastName[1] = "Mohammed";
+		LastName[2] = "Tariq";
+		LastName[3] = "Ali";
+		LastName[4] = "Said";
+		LastName[5] = "Saif";
+		LastName[6] = "Mannan";
+		LastName[7] = "Sami";
+		LastName[8] = "Mohammed";
+		LastName[9] = "Talib";
 
-//		Array of Items ID:
-		int[] quantity = new int[10];
-		quantity[0] = 6325;
-		quantity[1] = 3325;
-		quantity[2] = 5254;
-		quantity[3] = 321;
-		quantity[4] = 6945;
-		quantity[5] = 1250;
-		quantity[6] = 1234;
-		quantity[7] = 8675;
-		quantity[8] = 465;
-		quantity[9] = 7554;
+//		Array of customers Phone Number:
+		int[] PhoneNumber = new int[10];
+		PhoneNumber[0] = 96514485;
+		PhoneNumber[1] = 96543325;
+		PhoneNumber[2] = 98455254;
+		PhoneNumber[3] = 95623321;
+		PhoneNumber[4] = 96589945;
+		PhoneNumber[5] = 96961250;
+		PhoneNumber[6] = 96961234;
+		PhoneNumber[7] = 96158675;
+		PhoneNumber[8] = 97518465;
+		PhoneNumber[9] = 99665588;
+
+//		Array of number Items purchased:
+		int[] NoOfItems = new int[10];
+		NoOfItems[0] = 5;
+		NoOfItems[1] = 9;
+		NoOfItems[2] = 6;
+		NoOfItems[3] = 2;
+		NoOfItems[4] = 3;
+		NoOfItems[5] = 1;
+		NoOfItems[6] = 7;
+		NoOfItems[7] = 5;
+		NoOfItems[8] = 8;
+		NoOfItems[9] = 7;
+
+//		Array of Total amount:
+		double[] totalAmount = new double[10];
+		totalAmount[0] = 2.250;
+		totalAmount[1] = 9.980;
+		totalAmount[2] = 6.990;
+		totalAmount[3] = 2.520;
+		totalAmount[4] = 3.350;
+		totalAmount[5] = 1.120;
+		totalAmount[6] = 7.980;
+		totalAmount[7] = 5.950;
+		totalAmount[8] = 8.750;
+		totalAmount[9] = 7.350;
+
+//		Array of Total amount:
+		double[] PaidAmount = new double[10];
+		PaidAmount[0] = 5;
+		PaidAmount[1] = 10;
+		PaidAmount[2] = 7;
+		PaidAmount[3] = 5;
+		PaidAmount[4] = 5;
+		PaidAmount[5] = 2;
+		PaidAmount[6] = 10;
+		PaidAmount[7] = 7;
+		PaidAmount[8] = 10;
+		PaidAmount[9] = 10;
+
+		double[] Balance = new double[totalAmount.length];
+
+		for (int i = 0; i < totalAmount.length; i++) {
+			Balance[i] = PaidAmount[i] - totalAmount[i];
+		}
 
 		System.out.println("\n");
 
 //		To repeat the Invoice multiple times with counter:
-		for (int i = 1; i <= 11; i++) {
-			System.out.println(date);
-			System.out.println("Invoice NO: AB000" + counter);
-			System.out.println(getFirstName() + " " + getMiddleName() + " " + getLastName());
-			System.out.println("Phone Number: " + getPhoneNumber());
+		for (int i = 0; i < 10; i++) {
+			System.out.println("--------------------------------------------------");
+			System.out.println("Invoice Number: [0" + counter + "]");
+			System.out.println("Issued on: (" + date + ")");
+			System.out.println("Invoice NO: 000" + counter + 2 + counter + 5 + counter + 1);
+			System.out.println(FirstName[i] + " " + MiddleName[i] + " " + LastName[i]);
+			System.out.println("Phone Number: " + PhoneNumber[i]);
+			System.out.println("Number of Items Purchased: " + NoOfItems[i]);
+			System.out.println("Total Amount: " + totalAmount[i] + " OMR");
+			System.out.println("Total Amount Paid: " + PaidAmount[i] + " OMR");
+			System.out.println("Remaining Amount: " + Balance[i] + " OMR");
+			System.out.println("--------------------------------------------------");
 			System.out.println("\n");
 
 			counter++;
